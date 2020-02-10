348d7ad68dc30e42a3dc56241bb25d63138eba4e
CamilleJULIO <camille.julio@soprasteria.com>
2020-02-10 09:52:42 +0100

    ANGULAR SERVICE

--- LA_FACTORY_ANGULAR/lafactory/src/app/app-config.service.ts
+++ LA_FACTORY_ANGULAR/lafactory/src/app/app-config.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import{ HttpClientModule } from '@angular/common/http';
+import{ HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppConfigService {
+  public url: string = "http://localhost:8080/api/";
+  private myHeaders: HttpHeaders = new HttpHeaders();
+  private options: Object = null;//Pour sécurité à la fin
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  // public getQueryOption(): Object{//Pour sécurité à la fin
+  //   if(this.options == null){
+  //     this.myHeaders = this.myHeaders.append('Authorization', 'Basic ' + btoa('admin:123'));
+  //     this.options =  { headers: this.myHeaders };
+  //   }
+
+  //   return this.options;
+    
+  // }
 }
--- LA_FACTORY_ANGULAR/lafactory/src/app/app.module.ts
+++ LA_FACTORY_ANGULAR/lafactory/src/app/app.module.ts
@@ -29,6 +29,7 @@ const routes: Routes = [
     ModelComponent,
     AccueilComponent,
     RechercheComponent
+    ListModelComponent,
   ],
   imports: [
     BrowserModule,
