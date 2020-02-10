c9c19abcad85cac88439c0528c0c4cf0415b0286
de la Chesnais <thibaut.delachesnais@gmail.com>
2020-02-10 17:43:34 +0100

    detailModel

--- LA-Factory-boot/src/main/java/fr/formation/Controller/ModelesController.java
+++ LA-Factory-boot/src/main/java/fr/formation/Controller/ModelesController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.formation.dao.IDAOModele;
 
@@ -17,5 +19,12 @@ public class ModelesController {
 	public String findAll(Model model) {
 		model.addAttribute("modeles", daoModele.findAll());
 		return "listeModeles";
+	}
+	
+	@GetMapping("/detail/{modeleId}")
+	public String getDetail(@PathVariable int id,
+			Model model) {
+		model.addAttribute(daoModele.findById(id));
+		return null;
 	}
 }
