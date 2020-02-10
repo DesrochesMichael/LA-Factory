3349adaa940e8b4432a7f3552cdd19061b3be16d
de la Chesnais <thibaut.delachesnais@gmail.com>
2020-02-10 16:33:45 +0100

    idee archive

--- LA-Factory-data-jpa/src/main/java/fr/formation/dao/IDAOIdee.java
+++ LA-Factory-data-jpa/src/main/java/fr/formation/dao/IDAOIdee.java
@@ -1,9 +1,13 @@
 package fr.formation.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.formation.model.Idee;
 
 public interface IDAOIdee extends JpaRepository<Idee, Integer>  {
+	
+	public Optional<Idee> findByArchive(Boolean archive);
 
 }
