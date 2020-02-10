27bd6b280f0bfee2bf724fe3618c565ed4ab9709
de la Chesnais <thibaut.delachesnais@gmail.com>
2020-02-10 09:51:12 +0100

    connexionPage

--- LA-Factory-boot/src/main/java/fr/formation/Controller/HomeController.java
+++ LA-Factory-boot/src/main/java/fr/formation/Controller/HomeController.java
@@ -2,12 +2,23 @@ package fr.formation.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/home")
 	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/connexion")
+	public String goConnexion() {
+		return "connexion";
+	}
+	
+	@PostMapping("/connexion")
+	public String connexion() {
 		return "home";
 	}
 
