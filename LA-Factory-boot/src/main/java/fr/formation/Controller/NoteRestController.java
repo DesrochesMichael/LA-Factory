4bafd8018ccafacadd8b3f9d41a31f896a78f2ed
CamilleJULIO <camille.julio@soprasteria.com>
2020-02-11 11:30:11 +0100

    LLL

--- LA-Factory-boot/src/main/java/fr/formation/Controller/NoteRestController.java
+++ LA-Factory-boot/src/main/java/fr/formation/Controller/NoteRestController.java
@@ -1,17 +1,27 @@
 package fr.formation.Controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import fr.formation.dao.IDAONote;
+import fr.formation.model.Note;
+import fr.formation.views.Views;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/note")
 public class NoteRestController {
 	
-//	@Autowired
-//	IDAONote daonote;
-//
+	@Autowired
+	IDAONote daoNote;
+
 //	
 //	@GetMapping
 //	public List<Note> get() {
@@ -35,4 +45,17 @@ public class NoteRestController {
 //	public void delete(@PathVariable int id) {
 //		daonote.deleteById(id);
 //	}
+	
+	@GetMapping("/notemoyenne/{id}")
+	@JsonView(Views.Modele.class)
+	public void noteMoyenne(@PathVariable int id) {
+		List<Note> notes = daoNote.findByModeleId(id);
+		
+		float noteMoyenne = 0;
+		
+		for(int i=0; i<notes.size(); i++) {
+			noteMoyenne = noteMoyenne + notes.get(i).getValeur();
+		}
+	}
+	
 }
